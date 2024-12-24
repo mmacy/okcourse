86a58c77b2285f7275cfa0212b31693472b07b1e
Marsh Macy <mmacy@users.noreply.github.com>
2024-12-24 10:09:36 -0800

    API ref generation functional

--- scripts/gen_ref_docs.py
+++ scripts/gen_ref_docs.py
@@ -1,15 +1,14 @@
-"""Generates reference docs from the docstrings in the code.
-
-Adapted from https://github.com/mkdocstrings/mkdocstrings/blob/2f6ddb/scripts/gen_ref_nav.py
-"""
+"""Generate the code reference pages and navigation."""
 
 from pathlib import Path
 
 import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
+mod_symbol = '<code class="doc-symbol doc-symbol-nav doc-symbol-module"></code>'
 
-src = Path(__file__).parent / "src"
+root = Path(__file__).parent.parent
+src = root / "src"
 
 for path in sorted(src.rglob("*.py")):
     module_path = path.relative_to(src).with_suffix("")
@@ -25,18 +24,14 @@ for path in sorted(src.rglob("*.py")):
     elif parts[-1].startswith("_"):
         continue
 
-    nav_parts = [part for part in parts]
+    nav_parts = [f"{mod_symbol} {part}" for part in parts]
     nav[tuple(nav_parts)] = doc_path.as_posix()
-
-    # with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-    #     ident = ".".join(parts)
-    #     fd.write(f"::: src.{ident}")
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         ident = ".".join(parts)
         fd.write(f"---\ntitle: {ident}\n---\n\n::: {ident}")
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, ".." / path)
+    mkdocs_gen_files.set_edit_path(full_doc_path, ".." / path.relative_to(root))
 
 with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
